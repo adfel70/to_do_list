26836b75e1a5c2b242a1234c74745b140d7cdc24
adfel70 <adfel70@gmail.com>
2024-10-09 14:45:23 +0300

    merged

--- to_do_list.py
+++ to_do_list.py
@@ -1,4 +1,4 @@
-from typing import Optional
+import hashlib
 
 from bson import ObjectId
 from fastapi import FastAPI, HTTPException
@@ -6,6 +6,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import uvicorn
 from pydantic import BaseModel
 from datetime import date
+from typing import Optional
 
 app = FastAPI()
 
@@ -17,51 +18,66 @@ collection = db['list_1']
 class Task(BaseModel):
     # the id will be created separately
     name: str
-    priority: Optional[str]  # urgent, important or unimportant
-    expiration_date: Optional[date]
-    finished: Optional[bool]
-    remind: Optional[bool]
+    priority: Optional[str] = None  # urgent, important or unimportant
+    expiration_date: Optional[date] = None
+    finished: Optional[bool] = None
+    remind: Optional[bool] = None
+
 
 def bson_to_dict(task):
     task["_id"] = int(task["_id"])
     return task
 
 
-@app.post("/tasks/", response_model=dict)
+def get_id(x):
+    return hashlib.md5(x.encode('utf-8'))
+
+
+@app.post("/tasks/", response_model=str)
 async def create_task(task: Task):
-
-@app.put("", response_model=dict)
-async def update_expiration_date(task: Task):
-
-@app.put("", response_model=dict)
-async def update_status(task: Task):
-
-@app.put("", response_model=dict)
-async def update_priority(task: Task):
-
-@app.put("", response_model=dict)
-async def update_remind(task: Task):
+    new_task = {
+        "_id": get_id(task.name),
+        "name": task.name,
+        "priority": task.priority,
+        "expiration_date": task.expiration_date,
+        "remind": task.remind,
+        "finished": task.finished
+    }
+    db.collection.insert_one(new_task)
+    return f"received task successfully"
 
 
+@app.get("/tasks/{task_name}", response_model=dict)
+async def read_task(task_name: str):
+    task = await collection.find_one({"name": ObjectId(task_name)})
+    if task is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return bson_to_dict(task)
 
-@app.get("/tasks/{task_id}", response_model=dict)
-async def read_task(task_id: int):
-        task = await collection.find_one({"_id": ObjectId(task_id)})
-        if task is None:
-            raise HTTPException(status_code=404, detail="Item not found")
-        return bson_to_dict(task)
+    # @app.get("/tasks/", response_model=dict)
+    # async def create_task(task: Task):
+    #
+    # @app.get("/tasks/", response_model=dict)
+    # async def create_task(task: Task):
+    #
+    # @app.get("/tasks/", response_model=dict)
+    # async def create_task(task: Task):
+    #
+    # @app.get("/tasks/", response_model=dict)
+    # async def create_task(task: Task):
 
-        # @app.get("/tasks/", response_model=dict)
-            # async def create_task(task: Task):
-            # 
-            # @app.get("/tasks/", response_model=dict)
-            # async def create_task(task: Task):
-            # 
-            # @app.get("/tasks/", response_model=dict)
-            # async def create_task(task: Task):
-            # 
-            # @app.get("/tasks/", response_model=dict)
-            # async def create_task(task: Task):
+
+@app.put("/tasks/{task_name}", response_model=str)
+async def update_task(task_name: str, updated_task: Task):
+    old_task = collection.find_one({"name": task_name})
+    if not old_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    new_task = old_task.copy()
+    task_data = {k: v for k, v in updated_task.dist().items() if v is not None}
+    new_task.update(task_data)
+    collection.update_one({"name": task_name}, {"$set": new_task})
+    return f"update succeed"
+
 
 @app.delete("/tasks/{task_id}", response_model=dict)
 async def delete_task_by_id(task_id: int):
@@ -70,19 +86,19 @@ async def delete_task_by_id(task_id: int):
         raise HTTPException(status_code=404, detail="Item not found")
     return {"message": "Item deleted successfully"}
 
-@app.delete("/tasks/{task_description}", response_model=dict)
-async def delete_task_by_description(task_description : str):
-    result = await collection.delete_one({"_id": ObjectId(task_description)})
-    if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return {"message": "Item deleted successfully"}
+#
+# @app.delete("/tasks/{task_name}", response_model=dict)
+# async def delete_task_by_name(task_name: str):
+#     result = await collection.delete_one({"_id": ObjectId(task_name)})
+#     if result.deleted_count == 0:
+#         raise HTTPException(status_code=404, detail="Item not found")
+#     return {"message": "Item deleted successfully"}
 
-            # @app.delete("/tasks/", response_model=dict)
-            # async def create_task(task: Task):
-            #
-            # @app.delete("/tasks/", response_model=dict)
-            # async def create_task(task: Task):
-
+    # @app.delete("/tasks/", response_model=dict)
+    # async def create_task(task: Task):
+    #
+    # @app.delete("/tasks/", response_model=dict)
+    # async def create_task(task: Task):
 
 
 if __name__ == "__main__":
